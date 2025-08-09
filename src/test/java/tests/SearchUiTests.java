cd40d958d166a0d81b14e922f5a4b655928975b5
Tigr0K <yufing@mail.ru>
2025-08-09 15:51:40 +0300

    Удалил  лишнее и работает

--- src/test/java/tests/SearchUiTests.java
+++ src/test/java/tests/SearchUiTests.java
@@ -22,7 +22,7 @@ import static io.qameta.allure.Allure.step;
 
 
 @DisplayName("Параметризированные тесты на проверку поиска Wildberries")
-@Tag("SearchTest")
+@Tag("searchTests")
 public class SearchUiTests extends UiTestBase {
     MainPage mainPage = new MainPage();
 
